10af3f28a3fcfa17ea5d0a30ddaf4ec4bf0f4624
Nechar Joshi <nechar.joshi@healthdirect.org.au>
2025-08-15 22:16:46 +1000

    fix: build issues

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,13 +1,9 @@
 "use client";
 
-import { ChakraProvider } from "@chakra-ui/react";
-import { extendTheme } from "@chakra-ui/theme-tools"; // Correct import for extendTheme
+import { ChakraProvider } from "@chakra-ui/react";// Correct import for extendTheme
 import { ReactNode } from "react";
 
-const theme = extendTheme({
-  // Your custom theme configuration
-});
 
 export function Providers({ children }: { children: ReactNode }) {
-  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
+  return <ChakraProvider >{children}</ChakraProvider>;
 }
