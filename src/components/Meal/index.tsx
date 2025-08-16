9b2dcaf6508912a62d77b0717457ff5a36cfcce6
Nechar Joshi <nechar.joshi@healthdirect.org.au>
2025-08-16 14:23:35 +1000

    minor

--- src/components/Meal/index.tsx
+++ src/components/Meal/index.tsx
@@ -11,7 +11,7 @@ interface MealProps {
 const Meal: React.FC<MealProps> = ({ mealName, foods }) => {
   return (
     <Box p={4} borderWidth="1px" borderRadius="lg" mb={4}>
-      <Heading as="h2" size="lg" mb={2}>
+      <Heading as="h3" size="md" mb={2}>
         {mealName}
       </Heading>
       <FoodList foods={foods} />
