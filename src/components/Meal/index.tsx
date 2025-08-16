299b68acd101d7a13d34279cc2329480c649843c
Nechar Joshi <nechar.joshi@healthdirect.org.au>
2025-08-16 14:56:22 +1000

    wip

--- src/components/Meal/index.tsx
+++ src/components/Meal/index.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import { Box, Heading, Text } from "@chakra-ui/react";
-import FoodList from "../FoodList";
+import { Box, Heading,Text } from "@chakra-ui/react";
 import { Food } from "@/app/model/food-nutrient";
 
-import {  Grid } from "@chakra-ui/react";
+import { Grid } from "@chakra-ui/react";
 import FoodItem from "../FoodItem";
 
 interface MealProps {
@@ -12,24 +11,28 @@ interface MealProps {
 }
 
 const Meal: React.FC<MealProps> = ({ mealName, foods }) => {
-  
   return (
     <Box p={4} borderWidth="1px" borderRadius="lg" mb={4}>
       <Heading as="h3" size="md" mb={2}>
         {mealName}
       </Heading>
-
-       <Grid
-  templateColumns="repeat(3, 1fr)"
-  gap={4}
-  alignItems="start"   // ensures all items align at the top
->
-  {foods.map((food, index) => (
-    <Box key={index} h="100%">
-      <FoodItem food={food} />
-    </Box>
-  ))}
-</Grid>
+      {foods.length > 0 ? (
+        <Grid
+          templateColumns="repeat(3, 1fr)"
+          gap={4}
+          alignItems="start" // ensures all items align at the top
+        >
+          {foods.map((food, index) => (
+            <Box key={index} h="100%">
+              <FoodItem food={food} />
+            </Box>
+          ))}
+        </Grid>
+      ) : (
+        <Text  color={"gray.500"}>
+          Not loggged yet
+          </Text>
+      )}
     </Box>
   );
 };
