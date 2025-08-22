f51e442969fd7a13c07e8a5f9790d167d6b7a957
Nechar Joshi <nechar.joshi@healthdirect.org.au>
2025-08-22 13:42:41 +1000

    minor

---
+++ src/app/add-food/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Box } from "@chakra-ui/react";
+import React from "react";
+import AddFoodForm from "@/components/AddFoodForm";
+import MainLayout from "@/components/Layout";
+
+const Dashboard: React.FC = () => {
+  return (
+    <MainLayout title="Food Log">
+      <Box mb={8}>
+        <AddFoodForm />
+      </Box>
+    </MainLayout>
+  );
+};
+
+export default Dashboard;
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,20 +4,14 @@ import { Box } from "@chakra-ui/react";
 import React from "react";
 import Meal from "@/components/Meal";
 import Summary from "@/components/Summary";
-import AddFoodForm from "@/components/AddFoodForm";
 import MainLayout from "@/components/Layout";
 
 const Dashboard: React.FC = () => {
   return (
-    <MainLayout title="Food Log">
-      <Box mb={8}>
-        <AddFoodForm />
+    <MainLayout title="Summary">
+      <Box mb={3}>
+        <Summary />
       </Box>
-      <Box as="span" flex="1" textAlign="left" fontWeight="bold">
-        Daily Summary
-      </Box>
-
-      <Summary />
 
       <Box>
         <Meal mealName="Breakfast" />
