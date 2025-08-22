faab01d4c4b5199adb605db37bdb27ebc5785b29
Nechar Joshi <nechar.joshi@healthdirect.org.au>
2025-08-22 12:45:52 +1000

    feat: collect-user-information page

---
+++ src/app/collect-user-information/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CollectUserInformationPage = () => {
+  return (
+    <div>
+      <h1>Collect User Information</h1>
+      <p>This page is under construction.</p>
+    </div>
+  );
+};
+
+export default CollectUserInformationPage;
